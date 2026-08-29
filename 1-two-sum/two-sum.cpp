13019e9a0c9ea78a7610e35110a9ba357b2173d5
subhosahu <subhosahu7@gmail.com>
2026-08-29 17:05:59 +0530

    Time: 3 ms (66.99%) | Memory: 14.9 MB (26.67%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mpp;
+        for (int i = 0; i < nums.size(); i++) {
+            int a = nums[i];
+            int more = target - a;
+            if (mpp.find(more) != mpp.end()) {
+                return {mpp[more], i};
+            }
+            mpp[a] = i;
+        }
+        return {};
+    }
+};
