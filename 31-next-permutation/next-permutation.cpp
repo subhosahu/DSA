be087a69424d5844002c4ac5da0e2064a49c4290
subhosahu <subhosahu7@gmail.com>
2026-08-29 22:29:29 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (54.40%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -10,16 +10,20 @@ public:
                 break;
             }
         }
-        if(ind!=-1){
+        if(ind==-1){
+            reverse(nums.begin(),nums.end());
+        }
+        else {
             for (int i=n-1;i>=ind;i--){
                 if (nums[i]>nums[ind]){
                     swap(nums[i],nums[ind]);
                     break;
                 }
             }
+            reverse(nums.begin() + ind + 1, nums.end());
         }
         
-        reverse(nums.begin() + ind + 1, nums.end());
+       
         
     }
 };
